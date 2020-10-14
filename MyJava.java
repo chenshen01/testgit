0827160e4fc559f1905d9ea0298b15cc933acc5e
liuzhixiang <zhixiang.liu01@hand-china.com>
2020-10-14 22:35:08 +0800

    ww

--- MyJava.java
+++ MyJava.java
@@ -1,13 +1,7 @@
 public class ExecutorServiceDemo {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-
 		SysexMessageuat
 		SysexMessageuat01
-
-=======
-		SysexMessageuat01
 		hello
->>>>>>> uat_01
     }
 }
