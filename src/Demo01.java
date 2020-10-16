25185ce0e736aa3c51831a8d4ca9ee9ae0c75e88
liuzhixiang <zhixiang.liu01@hand-china.com>
2020-10-16 16:16:48 +0800

    [FIX]

---
+++ src/Demo01.java
@@ -0,0 +1,11 @@
+/**
+ * description
+ *
+ * @author liuzhixiang 2020/10/16 16:16
+ */
+public class Demo01 {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
